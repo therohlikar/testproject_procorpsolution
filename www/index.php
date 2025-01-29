150f7d6f168f808478e29747206fb44567b2840a
Radek Jeník <radek.jenik36@gmail.com>
2025-01-29 16:22:36 +0100

    refactor: index.php - apitte

--- www/index.php
+++ www/index.php
@@ -2,9 +2,12 @@
 
 declare(strict_types=1);
 
+use Apitte\Core\Application\Application as ApitteApplication;
+use App\Bootstrap;
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $bootstrap = new App\Bootstrap;
 $container = $bootstrap->bootWebApplication();
-$application = $container->getByType(Nette\Application\Application::class);
+$application = $container->getByType(ApitteApplication::class);
 $application->run();
