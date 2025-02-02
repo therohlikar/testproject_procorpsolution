2a9edc894027b2bbc652db97dd5c82acb8be1b3d
Radek Jeník <radek.jenik36@gmail.com>
2025-02-02 17:33:13 +0100

    feat: UserRepository

---
+++ app/Repository/UserRepository.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Repository;
+
+use App\Model\User;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+class UserRepository extends EntityRepository
+{
+
+}
