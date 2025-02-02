5b1955411c7fe504a4d8bc4cb77c4736f71c814a
Radek Jeník <radek.jenik36@gmail.com>
2025-02-02 18:25:40 +0100

    feat: debugger for all!

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -32,7 +32,7 @@ class Bootstrap
 
 	public function initializeEnvironment(): void
 	{
-		//$this->configurator->setDebugMode('secret@23.75.345.200'); // enable for your remote IP
+		$this->configurator->setDebugMode(true);
 		$this->configurator->enableTracy($this->rootDir . '/log');
 
 		$this->configurator->createRobotLoader()
