85833ac720aecec912c22a6ea98841d00948aab3
Radek Jeník <radek.jenik36@gmail.com>
2025-02-02 17:29:58 +0100

    feat: BaseController

---
+++ app/Controller/BaseController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use Apitte\Core\Annotation\Controller\Path;
+use Apitte\Core\UI\Controller\IController;
+
+/**
+ * @Path("/api")
+ */
+abstract class BaseController implements IController
+{
+
+}
